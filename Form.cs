1d905b2c12377f2c02e9403cf4191f4232553b5b
shbz1690 <msk90@georgetown.edu>
2016-01-08 09:05:28 -0500

    Update and rename .gitignore to Form.cs

---
+++ Form.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DNA;
+
+namespace mainpage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+            label14.Visible = false;
+            label15.Visible = false;
+            label16.Visible = false;
+            label17.Visible = false;
+            label18.Visible = false;
+            label19.Visible = false;
+            label20.Visible = false;
+            label21.Visible = false;
+            textBox2.Clear();
+            string rna=Main_operation.DNA_To_RNA(textBox1.Text);
+            textBox2.Text = rna;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+            label14.Visible = false;
+            label15.Visible = false;
+            label16.Visible = false;
+            label17.Visible = false;
+            label18.Visible = false;
+            label19.Visible = false;
+            label20.Visible = false;
+            label21.Visible = false;
+            textBox2.Clear();
+            string dna = Main_operation.RNA_To_DNA(textBox1.Text);
+            textBox2.Text = dna;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+            label14.Visible = false;
+            label15.Visible = false;
+            label16.Visible = false;
+            label17.Visible = false;
+            label18.Visible = false;
+            label19.Visible = false;
+            label20.Visible = false;
+            label21.Visible = false;
+            textBox2.Clear();
+            string dna_comp = Main_operation.DNA_complementry(textBox1.Text);
+            textBox2.Text = dna_comp;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+            label14.Visible = false;
+            label15.Visible = false;
+            label16.Visible = false;
+            label17.Visible = false;
+            label18.Visible = false;
+            label19.Visible = false;
+            label20.Visible = false;
+            label21.Visible = false;
+            textBox2.Clear();
+            string rna_comp = Main_operation.RNA_complementry(textBox1.Text);
+            textBox2.Text = rna_comp;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+            label14.Visible = false;
+            label15.Visible = false;
+            label16.Visible = false;
+            label17.Visible = false;
+            label18.Visible = false;
+            label19.Visible = false;
+            label20.Visible = false;
+            label21.Visible = false;
+            textBox2.Clear();
+            string rev_seq = Main_operation.Reversion(textBox1.Text);
+            textBox2.Text = rev_seq;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+            label9.Visible = true;
+            label10.Visible = true;
+            label11.Visible = true;
+            label12.Visible = true;
+            label13.Visible = true;
+            label14.Visible = true;
+            label15.Visible = true;
+            label16.Visible = true;
+            label17.Visible = true;
+            label18.Visible = true;
+            label19.Visible = true;
+            label20.Visible = true;
+            label21.Visible = true;
+            float[] a=Main_operation.Nu_Percentage(textBox1.Text);
+            label3.Text =Convert.ToString(a[0]);
+            label5.Text = Convert.ToString(a[1]);
+            label7.Text = Convert.ToString(a[2]);
+            label9.Text = Convert.ToString(a[3]);
+            label11.Text = Convert.ToString(a[4]);
+            label13.Text = Convert.ToString(a[5]);
+            label15.Text = Convert.ToString(a[6]);
+            label17.Text = Convert.ToString(a[7]);
+            label19.Text = Convert.ToString(a[8]);
+            label21.Text = Convert.ToString(a[9]);
+            
+            //float[] a= Main_operation.Nu_Percentage(textBox1.Text);
+            //listBox1.DataSource =a;
+        }
+    }
+}
+
